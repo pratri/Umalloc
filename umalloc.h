7c5fb22baedbb8546fe48e8f2bfe19904fb67e0d
pratri <44682329+pratri@users.noreply.github.com>
2022-11-22 17:32:00 -0500

    Add files via upload

---
+++ umalloc.h
@@ -0,0 +1,21 @@
+#ifndef _UMALLOC_H_
+#define _UMALLOC_H_
+#define malloc(X) umalloc(X, __FILE__, __LINE__)
+#define free(Y) ufree(Y, __FILE__, __LINE__)
+
+
+typedef struct _listnode{
+    //Size in bytes
+    size_t size;
+    //if 0 free if 1 taken
+    int free;
+    struct _listnode *next;
+    struct _listnode *prev;
+} ListNode;
+
+
+void *umalloc(size_t bytes, char *file, int line);
+void ufree(void *ptr, char *file, int line);
+void printMemory();
+
+#endif
