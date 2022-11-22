e61c68206ff83f0eb96dc9aa183f75541c5764c5
pratri <44682329+pratri@users.noreply.github.com>
2022-11-22 17:32:11 -0500

    Add files via upload

---
+++ memgrind.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "umalloc.h"
+
+int main(int argc, char *argv[]){
+
+    //Consistency
+
+   int* test1 = malloc(sizeof(int));
+   *test1 = 4;
+   free(test1);
+   printf("test1: %p\n", test1);
+   int* test2 = malloc(sizeof(int));
+   *test2 = 123;
+   free(test2); 
+   printf("test2: %p\n", test2);
+
+    //Maximazation
+    void* test;
+    int size = 1;
+    test = malloc(1);
+    while(test!=NULL){
+        free(test);
+        size = size * 2;
+        test = malloc(size);
+    }
+    size = size/2;
+    test = malloc(size);
+    if(test!=NULL){
+        printf("%d\n", size);
+    }
+    free(test);
+
+    //Basic Coalescense
+
+    
+
+    //Saturation
+
+    //Time Overhead
+
+    //Intermediate Coallescense
+
+
+
+}
